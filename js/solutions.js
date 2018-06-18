9b6e8095feb11e03262336370ac95838a398b4f0
Erica Bruenton <ercfit@gmail.com>
2018-06-17 20:02:08 -0600

    Commit 2 - Palindrome

---
+++ js/solutions.js
@@ -0,0 +1,23 @@
+//1. file made
+
+//2. parameter: type of variable
+//argument: value of the parameter
+
+//3.return: gives a function it's value and stops a function--we can use the return value
+//of a function to pass as data
+//console.log(): prints to the cosole--no affect on data
+
+
+
+//Palindrome (Ryan went over this in class with us)
+// const checkPalindrome = (word) => {
+// 	let reverseString = word.split("").reverse().join("");
+// 		if(word.toLowerCase() == reverseString.toLowerCase()){
+// 			return true;
+// 		}else{
+// 			return false;
+// 			}
+// }
+// 	console.log(checkPalindrome("Radar"));
+// 	console.log(checkPalindrome("Borscht"));
+
